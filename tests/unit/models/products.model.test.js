50199bf4f55eed6cfd91bd6b595f8825a194faee
Alex <alexsandrolima991@hotmail.com>
2022-11-11 20:32:12 -0300

    Requisito 2 - Verificação se está tudo correto

---
+++ tests/unit/models/products.model.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { productsModel } = require('../../../src/models');
+
+const connection = require('../../../src/models/connection');
+const { products } = require('../models/mocks/products.model.mock');
+
+describe('Testes de unidade do model de products', function () {
+  afterEach(sinon.restore);
+
+  it('Recupera a lista de produtos', async function () {
+    sinon.stub(connection, 'execute').resolves([products]);
+
+    const result = await productsModel.getAll();
+
+    expect(result).to.be.deep.equal(products);
+  });
+
+  it('Recupera a lista de produtos a partir do array', async function () {
+    sinon.stub(connection, 'execute').resolves([[products[0]]]);
+
+    const result = await productsModel.getById(1);
+
+    expect(result).to.be.deep.equal(products[0]);
+  })
+})
